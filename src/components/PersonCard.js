bfeedba39687d1f28c77790126656553769ac0e4
Danniel C <dnncervantes.yahoo.com>
2022-05-04 18:02:34 -0700

    first commit

---
+++ src/components/PersonCard.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class PersonCard extends Component {
+
+
+    
+    render() {
+        const {lastName, firstName, age, hairColor} = this.props;
+        return(
+            <div>
+                <h1>
+                {this.props.lastName}, {this.props.firstName}
+                </h1>
+                <p>Age: {this.props.age}</p>
+                <p>Hair Color: {this.props.hairColor}</p>
+            </div>
+        )
+    }
+}
+
+export default PersonCard;
